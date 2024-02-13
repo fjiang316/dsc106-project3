99377bd22ee806eddc784bc1744c705f704ea338
evelynh037 <emily.hxqxxx@gmail.com>
2024-02-13 00:37:44 -0800

    add do representing cities to map

--- project3/todomvc/src/components/App.svelte
+++ project3/todomvc/src/components/App.svelte
@@ -1,51 +1,73 @@
 <script>
-	// https://github.com/topojson/us-atlas
-	// https://github.com/d3/d3-geo
-	// https://observablehq.com/@mbostock/u-s-state-map
-	// TODO: https://observablehq.com/@d3/u-s-map
-	// TODO: https://observablehq.com/@jeantimex/us-state-county-map
-	
 	import { onMount } from 'svelte';
 	import * as topojson from 'topojson-client';
 	import { geoPath, geoAlbersUsa } from 'd3-geo';
 	import { draw } from 'svelte/transition';
 	import * as d3 from 'd3';
 
-	//load data
+	const projection = geoAlbersUsa().scale(1300).translate([487.5, 305])
+	const path = geoPath();
+	let tooltip = { city_name: "", foreign: "", visibility: 'hidden', x: 0, y: 0  };
+	let states = [];
+	let cities = [];
+	let selected;
+
+	//Load data
 	onMount(async () => {
     try {
-      const cities = await d3.csv("repeatedCities.csv");
-      console.log(cities);
+      cities = await d3.csv("repeatedCities.csv");
+	  console.log(cities);
     } catch (error) {
       console.error("Error loading the CSV file:", error);
     }
   });
-	
 
-	// https://github.com/topojson/us-atlas#us-atlas-topojson
-	const projection = geoAlbersUsa().scale(1300).translate([487.5, 305])
-	
-	const path = geoPath().projection(null);
-	
-	let states = [];
-	let selected;
-	
+  //US map
 	onMount(async () => {
 		const us = await fetch('https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json')
 			.then(d => d.json())
 		console.log({ us })
-		
 		states = topojson.feature(us, us.objects.states).features;
+		
 	})
+
+	//Tooltip
+	function showTP(city) {
+        tooltip.city_name = city.city_ascii;
+		tooltip.foreign = city.country_foreign;
+        tooltip.x = projection([city.lng_us, city.lat_us])[0]; 
+        tooltip.y = projection([city.lng_us, city.lat_us])[1]; 
+        tooltip.visibility = 'visible';
+    }
+    function hideTP() {
+        tooltip.visibility = 'hidden';
+    }
 </script>
 
-<svg viewBox="0 0 975 610">
-	<!-- State shapes -->
-	<g fill="white" stroke="black">
+<svg width="900" height="570" viewBox="0 0 975 610">
+	<g class = "map" fill="#F2F2F2" stroke="grey">
 		{#each states as feature, i}
 			<path d={path(feature)} on:mouseover={() => selected = feature} class="state" in:draw={{ delay: i * 50, duration: 1000 }} />
 		{/each}
-	</g>	
+	</g>
+	<g class="cities" fill="grey" stroke="black">
+		{#each cities as city}
+			<circle cx={projection([city.lng_us, city.lat_us])[0]} cy={projection([city.lng_us, city.lat_us])[1]}
+			 on:mouseover={showTP(city)} on:mouseout={hideTP} r="3.7" />
+		{/each}
+		<rect 
+        x={tooltip.x +9} 
+        y={tooltip.y - 38} 
+		width = {(tooltip.foreign.length +8)* 8.5}
+        height={55} 
+        fill="#B8DCA8" 
+        stroke="grey" 
+        visibility={tooltip.visibility} />
+        <text class="tooltip" y={tooltip.y - 30} visibility={tooltip.visibility}  stroke= None>
+			<tspan x={tooltip.x + 15} dy="1.1em"> {"Name: " + tooltip.city_name} </tspan>
+			<tspan x={tooltip.x + 15} dy="1.1em"> {"Foreign: " + tooltip.foreign} </tspan>
+		</text>
+	</g>
 </svg>
 
 <style>
@@ -58,6 +80,11 @@
       border: 1px solid black;
       border-radius: 1px;
       font-size: 25px;
+    }
+	.tooltip {
+        font-size: 14px;
+		fill: rgb(2, 100, 12);
+		font-weight: bold;
     }
   </style>
   
