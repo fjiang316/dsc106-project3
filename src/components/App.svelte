e5e2766cd42e7115cee0e5e6b2a06576fb0968c8
evelynh037 <emily.hxqxxx@gmail.com>
2024-02-17 01:42:51 -0800

    description added

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -4,6 +4,7 @@
 	import { geoPath, geoAlbersUsa } from 'd3-geo';
 	import { draw } from 'svelte/transition';
 	import * as d3 from 'd3';
+	import { zoom } from 'd3-zoom';
 
 	//import { writable } from 'svelte/store';
 	//import {showEnglish, showArabic,showDutch,showFrench,showGerman,showItalian,showPortuguese, showSpanish} from './checkboxStore';
@@ -14,6 +15,7 @@
 	let cities = [];
 	let selected;
 	let description = '';
+	let message = "";
 	let zoomLevel = 1;
 	
 	let showEnglish = false; // checkbox for yes
@@ -54,6 +56,7 @@
 	function showTP(city) {
         tooltip.city_name = city.city_ascii;
 		tooltip.foreign = city.country_foreign;
+		tooltip.state = city.admin_name_us;
 		const xCoordinate = projection([city.lng_us, city.lat_us])[0];
     	const tooltipWidth = (tooltip.foreign.length + 20) * 8.5;
     	tooltip.x = xCoordinate + tooltipWidth > 975 ? xCoordinate - tooltipWidth : xCoordinate;
@@ -68,21 +71,34 @@
 
 	$: {
 		let selectedLanguages = [];
+		let selectedM = [];
 
 		if (showEnglish) selectedLanguages.push('');
 		if (showSpanish) selectedLanguages.push('');
-		if (showArabic) selectedLanguages.push('Arabic');
+		if (showArabic) selectedLanguages.push('');
 		if (showDutch) selectedLanguages.push('');
 		if (showFrench) selectedLanguages.push('');
 		if (showGerman) selectedLanguages.push('');
 		if (showItalian) selectedLanguages.push('');
 		if (showPortuguese) selectedLanguages.push('');
 
+		if (showEnglish) selectedM.push('As we can observe, the majority of cities that share their names with English-speaking countries are located in the eastern United States, particularly in states like MA, PA, NY, and NJ. Among these countries, the UK is the most common source of city names due to the strong influence of British culture and settlers during the early colonization period.');
+		if (showSpanish) selectedM.push("Many US cities sharing names with Spanish-speaking countries are located in the state of CA and south part of US. The pattern can be attributed to the historical Spanish colonization and influence in these regions");
+		if (showArabic) selectedM.push("As we can observe, the majority of cities that share their names with Arabic-speaking countries are located in the eastern US. The names of these cities, such as Cairo, Medina, and Alexandria, are major cities of Egypt and Saudi Arabia. Majority of the name came from Greek mythology.");
+		if (showDutch) selectedM.push('As we can observe, the majority of cities that share their names with Dutch-speaking countries are located along the east cost where the first Dutch colony in Noth America, New Netherland, was at. Netherland is the most common source and city like Amsterdam, NY is named in honor of early Dutch settlers.');
+		if (showFrench) selectedM.push('Many US cities sharing names with French-speaking countries are located in the East, especially in states near Canada, extending into the Midwest where French colonies once were. Canada is the most common source and it is also a former French colony. The close geographical proximity and historical ties can also be reason to the pattern.');
+		if (showGerman) selectedM.push('Many US cities sharing names with German-speaking countries are located in the northeast(NY, VA etc). Germany and Austria are two major countries and the pattern may be associated with the largest flow of German immigration to America occurred between 1820 and World War I to the east');
+		if (showItalian) selectedM.push('As we can observe, the majority of cities that share their names with Italian-speaking countries are located in the east of US. The city name Florance and country Italy appeared the most in the graph. The influx of Italian immigrats at the end of 19th century and the begining of 20th century.');
+		if (showPortuguese) selectedM.push('Many US cities sharing names with Portuguese-speaking countries is located in CA. Portugal and Brazil, a formor Portugal colony, are two main countries. The historical reason for this pattern can be due to the California gold rush. During that time, there is significant flow of Portuguese Immigrants to California.');
+
 		if (selectedLanguages.length > 0) {
 		// If at least one checkbox is checked, update the description
-		description = `${selectedLanguages.join('\n ')}`;
+		message = selectedM;
+		description = `${selectedLanguages.join('')}`;
+		
 		} else {
 		// If no checkbox is checked, provide another default message
+		message = ""
 		description = 'Please select the checkbox on the right side to see cities in the US that have their name from a specific foreign language origin. For ease of pattern recognization, please select the box one at a time first.';
 		}
 
@@ -90,6 +106,7 @@
     	for (const city of cities) {
       		city.color = getDotColor(city);
     	}
+		console.log(selectedLanguages)
   	}
 	// checkbox change handle
 	function handleCheckboxChange(e, language) {
@@ -150,14 +167,33 @@
 			return 'lightgrey';
 		}
 	}
+	onMount(async () => {
+    const svg = d3.select('#map');
+
+    // Define the zoom behavior
+    const zoomBehavior = zoom()
+        .scaleExtent([1, 8])
+        .on('zoom', (event) => {
+            svg.select('.map-container').attr('transform', event.transform);
+            svg.select('.cities-container').attr('transform', event.transform);
+        });
+
+    // Apply the zoom behavior to the SVG
+    svg.call(zoomBehavior);
+
+    const us = await fetch('https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json')
+        .then(d => d.json());
+    console.log({ us });
+    states = topojson.feature(us, us.objects.states).features;
+});
 </script>
 
-<h1>Pattern of US City Names: Does it show pattern of culture immegration in US history?</h1>
-
-<svg width="900" height="570" viewBox="0 0 975 610" style ={{ maxWidth: '100%', height: 'auto' }}>
-	<g class="map-container" fill="#F2F2F2" stroke="grey">
+<h1>Pattern of US City Names: Does it show pattern of culture migration and immigration in US history?</h1>
+<br>
+<svg  id="map" width="900" height="570" viewBox="0 0 975 610" style ={{ maxWidth: '100%', height: 'auto' }}>
+	<g class="map-container" fill="white" stroke="grey">
 		{#each states as feature, i}
-		<path d={path(feature)} on:mouseover={() => selected = feature} class="state" in:draw={{ delay: i * 50, duration: 1000 }} />
+		<path d={path(feature)} on:mouseover={() => selected = feature} class="state" in:draw={{ delay: i * 50, duration: 1000 }}/>
 		{/each}
 	</g>
 
@@ -169,7 +205,7 @@
 			cy={projection([city.lng_us, city.lat_us])[1]}
 			on:mouseover={() => { showTP(city); city.highlighted = true; }}
 			on:mouseout={() => { hideTP(); city.highlighted = false; }}
-			r="3.7"
+			r="4"
 			fill={city.highlighted ? 'green' : city.color}
 			/>
 		{/if}
@@ -179,19 +215,24 @@
 		x={tooltip.x +9} 
 		y={tooltip.y - 38} 
 		width = {(tooltip.foreign.length +12)* 8.5}
-		height={55} 
+		height={65} 
 		fill="#B8DCA8" 
 		stroke="grey" 
 		visibility={tooltip.visibility} />
 		<text class="tooltip" y={tooltip.y - 30} visibility={tooltip.visibility}  stroke= None>
 			<tspan x={tooltip.x + 15} dy="1.1em"> {"City Name: " + tooltip.city_name} </tspan>
+			<tspan x={tooltip.x + 15} dy="1.1em"> {"State: " + tooltip.state} </tspan>
 			<tspan x={tooltip.x + 15} dy="1.1em"> {"Foreign Origins: " + tooltip.foreign} </tspan>
 		</text>
 </svg>
 
 <div class='checkbox-container'>
-	<b>Frequently Seen Immegrating Culture Languages</b>
-	<u> {description}</u>
+	<br>
+	<br>
+	<b>Frequently Seen Immigrating Culture Languages</b>
+	<br>
+	<a1> {description}</a1>
+	<br>
 	<label>
 		<input type="checkbox" bind:checked={showEnglish} on:change={(e) => handleCheckboxChange(e, 'English')}/>
 		Show English
@@ -224,10 +265,26 @@
 		<input type="checkbox" bind:checked={showPortuguese} on:change={(e) => handleCheckboxChange(e, 'Portuguese')}/>
 		Show Portuguese
 	</label>
+	{#each message as msg}
+	<br>
+	    <a > {msg}</a>
+	{/each}
 </div>
 
 
 <style>
+	h1 {
+    text-align: center;
+}
+    b{
+		font-size: 20px;
+	}
+	label{
+		font-weight: bold;
+	}
+	a{
+		color:red;
+	}
     .selectedName {
       position: absolute;
       top: 580px;
